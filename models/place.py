8baeb09db29607eccff7f4fd3b1a8dbc1d3b8d56
lei-diva <divaaleii@gmail.com>
2019-12-18 10:03:50 +0000

    fix name

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
                              ForeignKey('places.id'),
                              primary_key=True, nullable=False),
-                      Column('amenities_id', String(60),
+                      Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
                              primary_key=True, nullable=False))
 
