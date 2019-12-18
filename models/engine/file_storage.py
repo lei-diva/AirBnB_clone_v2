b7f2665b3b04b7d633082e67737a84dcb8cc898e
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-12-18 02:38:19 +0000

    added updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        copy_dictionary = {}
+        if cls is not None:
+            for key, value in self.__objects.items():
+                if self.__class__.__name__ == cls:
+                    copy_dictionary[key] = value
+            return copy_dictionary
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it is inside
+        """
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
+            self.save()
