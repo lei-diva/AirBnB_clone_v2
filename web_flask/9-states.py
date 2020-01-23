13c3c8049fe7021c4ecc24c1db430bfaf24e4444
lei-diva <divaaleii@gmail.com>
2020-01-23 07:40:03 +0000

    9

---
+++ web_flask/9-states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route("/states/", strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def state(id=None):
+    s = storage.all(State).values()
+    return render_template("9-states.html", states=s, id=id)
+
+
+@app.teardown_appcontext
+def tear(self):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
